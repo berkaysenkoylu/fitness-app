2dd49c6ea13e8669dda46a46e644ac9c6f14c674
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2019-09-26 15:16:26 +0300

    Updates

---
+++ precache-manifest.076fcac60870e213c3c5a212a48ccfe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494a61811497a89b675c9e066b069fce",
+    "url": "/fitness-app/index.html"
+  },
+  {
+    "revision": "65672cec3670cf50ff94",
+    "url": "/fitness-app/static/css/main.3cdf0f43.chunk.css"
+  },
+  {
+    "revision": "0b985848346b56d5fd80",
+    "url": "/fitness-app/static/js/2.03fb14cd.chunk.js"
+  },
+  {
+    "revision": "65672cec3670cf50ff94",
+    "url": "/fitness-app/static/js/main.89eed5bf.chunk.js"
+  },
+  {
+    "revision": "c7466b20f5d2ae4f22e0",
+    "url": "/fitness-app/static/js/runtime~main.e53bad42.js"
+  },
+  {
+    "revision": "84e6a04af9d1ab067b609c4cec60f018",
+    "url": "/fitness-app/static/media/logo.84e6a04a.png"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/fitness-app/static/media/yoga-nature.6e09f410.jpg"
+  }
+]);
